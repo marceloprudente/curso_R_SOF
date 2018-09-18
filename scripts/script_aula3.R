0ea7a92faf10601e0fc2e05f287b94f6a81906f8
marceloprudente <33759575+marceloprudente@users.noreply.github.com>
2018-09-18 08:59:43 -0300

    Create script_aula3.R

---
+++ scripts/script_aula3.R
@@ -0,0 +1,31 @@
+# Manipulação de dados com *dplyr*
+
+## limpar ambiente global
+rm(list = ls())
+
+## Primeiro passo: fixar diretório
+setwd("C:/curso_r/dados")
+
+## Segundo passo: utilizar pacotes
+library(tidyverse)
+
+## Terceiro passo: carregar dados sociais
+ds <- fread("dados_sociais.csv", 
+            dec = ',')
+
+## Quarto passo passo: analisar os dados
+glimpse(ds)
+summary(ds)
+
+# Verbos do *dplyr*
+
+## filter: selecionar linhas
+ds_2000 <- filter(ds, ano == 2000)
+ds_aracajives <- filter(ds, 
+                        municipio == "ARACAJU")
+
+ds_sp <- filter(ds, municipio == "SÃO PAULO")
+
+ds2 <- filter(ds, (ano == 2000  | ano == 1991) &
+         uf == 35 & 
+         esp_vida >= 70) 
