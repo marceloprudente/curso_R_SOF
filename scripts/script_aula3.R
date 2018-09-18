edc55d8eb2fbc80b513c6204b091818c6e4136df
marceloprudente <33759575+marceloprudente@users.noreply.github.com>
2018-09-18 09:01:44 -0300

    Update script_aula3.R

--- scripts/script_aula3.R
+++ scripts/script_aula3.R
@@ -17,15 +17,19 @@ ds <- fread("dados_sociais.csv",
 glimpse(ds)
 summary(ds)
 
-# Verbos do *dplyr*
+# Verbos do *dplyr*----
 
 ## filter: selecionar linhas
-ds_2000 <- filter(ds, ano == 2000)
-ds_aracajives <- filter(ds, 
-                        municipio == "ARACAJU")
 
+## apenas ano 2000
+ds_2000 <- filter(ds, ano == 2000)
+## apenas municipio de aracaju
+ds_ara <- filter(ds, 
+                        municipio == "ARACAJU")
+## ou municipio de São Paulo
 ds_sp <- filter(ds, municipio == "SÃO PAULO")
 
+## ano 1991 ou 2000, estado de são paulo e esperança de vida maior que 70 anos
 ds2 <- filter(ds, (ano == 2000  | ano == 1991) &
          uf == 35 & 
          esp_vida >= 70) 
