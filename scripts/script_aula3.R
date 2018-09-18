4e4c681e8c986b2f8bdce37650c04a00ad9f691f
marceloprudente <33759575+marceloprudente@users.noreply.github.com>
2018-09-18 11:12:45 -0300

    Update script_aula3.R

--- scripts/script_aula3.R
+++ scripts/script_aula3.R
@@ -8,6 +8,7 @@ setwd("C:/curso_r/dados")
 
 ## Segundo passo: utilizar pacotes
 library(tidyverse)
+library(data.table) # para fread()
 
 ## Terceiro passo: carregar dados sociais
 ds <- fread("dados_sociais.csv", 
@@ -33,3 +34,119 @@ ds_sp <- filter(ds, municipio == "SÃO PAULO")
 ds2 <- filter(ds, (ano == 2000  | ano == 1991) &
          uf == 35 & 
          esp_vida >= 70) 
+
+## Exercício: observe o banco dados_sociais tal que:
+### o ano seja igual a 2010
+### a taxa de analfabetismo seja maior que a média
+### o estado seja do nordeste
+
+ds_e1<- filter(ds, ano == 2010 &
+         tx_analf_15m > mean(tx_analf_15m) &
+         (uf >= 20 & uf <30 ))
+# verificar se selecionei NE
+table(ds_e1$uf)
+
+## filtrar texto
+
+ds_sao<-filter(ds, grepl("São", municipio,
+                 ignore.case = TRUE))
+
+# select() ----
+
+# selecionar 3 colunas
+ds_select <- select(ds, ano, uf, rdpc)
+
+head(ds_select)
+
+# retirar colunas
+ds_select2 <- select(ds, -rdpc, - pop)
+head(ds_select2)
+
+# com vetor
+nome <- c("ano", "uf", "pop")
+ds_select3<- select(ds, nome)
+head(ds_select3)
+
+# selecionar primeras
+ds_select4 <- select(ds, ano:municipio, -cod_ibge)
+head(ds_select4)
+
+# selecionar intervalo de colunas
+ds_select5 <- select(ds, 1:5)
+head(ds_select5)
+
+# selecionar cadeia, exceto 1
+ds_select6 <- select(ds, ano:municipio, -cod_ibge)
+head(ds_select6)
+
+# reordenar
+ds_select7 <- select(ds, 5:8, 1:4)
+head(ds_select7)
+
+ds_select8 <- select(ds, pop, everything())
+head(ds_select8)
+
+## select helpers
+sihsus <- read.dbc::read.dbc("RDSE1701.dbc")
+glimpse(sihsus)
+summary(sihsus)
+
+### selecionar colunas que começam com VAL
+sihsus_2 <- select(sihsus, 1:5,
+                   starts_with("val"))
+summary(sihsus_2)
+
+### renomear
+rename(ds, ANO = ano)
+#################################
+## PIPE - seu melhor amigo ----
+#################################
+
+x <- 1:20
+# forma tradicional
+round(log(x), 2)
+
+# com o pipe
+x %>% log() %>% round(2)
+
+# pipe com dplyr
+ds_e2 <- ds %>% 
+  select(ano, uf, rdpc) %>% 
+  filter(ano == 2010)
+
+# arrange
+ds_e2 <- ds %>% 
+  select(ano, uf, rdpc) %>% 
+  filter(ano == 2010) %>% 
+  arrange(-uf)
+head(ds_e2)
+
+ds_e3 <- ds %>% 
+  select(ano, uf, rdpc, pop) %>% 
+  filter(ano == 2010) %>% 
+  arrange(-pop, -uf) 
+head(ds_e3)
+
+
+ds %>% 
+  select(ano, uf, rdpc, pop) %>% 
+  filter(ano == 2010) %>% 
+  arrange(-pop) %>% 
+  head(10)
+
+# mutate(): criar novas variaveis
+ds_3 <- ds %>% 
+  select(ano, uf, rdpc, pop) %>% 
+  filter(ano == 2010) %>%
+  mutate(renda_total = rdpc * pop, 
+         log_pop = log(pop),
+         renda_sq = renda_total ^ 2)
+
+## group by e summarise
+esp_vida <- ds %>% 
+  group_by(ano, uf) %>% 
+  summarise(media = mean(esp_vida),
+            mediana = median(esp_vida),
+            populacao = sum(pop))
+
+
