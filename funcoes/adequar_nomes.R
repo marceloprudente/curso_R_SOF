559927bf0fb369c763a7109b0e231f8cc51a4a23
Marcelo Prudente <33759575+marceloprudente@users.noreply.github.com>
2020-01-17 14:26:56 -0300

    Delete adequar_nomes.R

--- funcoes/adequar_nomes.R
+++
@@ -1,8 +0,0 @@
-# Função para editar nomes
-adequar_nomes<- function(x){
-  require(stringr)
-  require(stringi)
-  x = stri_trans_general(x, "Latin-ASCII")
-  x = tolower(x)
-  x = str_replace_all(x, "[[:punct:][:space:]]", "_")
-}
